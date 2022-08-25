6f322c2adcc438109f4345f796883e5090bd4ba5
ChillChewie <99365759+ChillChewie@users.noreply.github.com>
2022-08-25 15:50:15 -0500

    Update push_config_ssh.py

--- push_config_ssh.py
+++ push_config_ssh.py
@@ -1,29 +1,30 @@
 import paramiko
 import time
 import os
-import maskpass
+from getpass import getpass
 from hosts import network_devices
 from configs import commands
 
 
 user = input("Username : ")
-pwd = maskpass.advpass() 
-
+pwd = getpass() 
+print(pwd)
 
 # For loop allows you to specify number of hosts
 for ip in  network_devices:
+    print(ip)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip, port=22, username=user, password=pwd)
-    remote = twrssh.invoke_shell()
-    remote.send('term len 0\n')
+    ssh.connect(ip, port=22, username=user, password=str(pwd))
+    connection = ssh.invoke_shell()
+    connection.send('term len 0\n')
     time.sleep(1)
     for command in commands:
-        remote.send(' %s \n' % command)
+        connection.send(' %s \n' % command)
         time.sleep(2)
-        buf = remote.recv(65000)
+        buf = connection.recv(65000)
         f = open('logfile.txt', 'a')
         f.write(str(buf))
         f.close()
-    twrssh.close()
-
+    print('done')
+    ssh.close()
